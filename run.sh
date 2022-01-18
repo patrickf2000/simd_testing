b5f7e6818908a754f74312e025f973913b551c66
Patrick Flynn <patrickf4664@protonmail.com>
2022-01-18 11:24:16 -0500

    Try new CSV format

--- run.sh
+++ run.sh
@@ -6,23 +6,34 @@
 #
 # Param 1- the program being run
 function run_intel() {
-    printf "$1," 1>> intel_explicit.csv
-    ./$1/$1"1" 1>> intel_explicit.csv
+    #printf "$1," 1>> intel_explicit.csv
+    #./$1/$1"1" 1>> intel_explicit.csv
     
-    printf "$1," 1>> intel_knl.csv
-    ./$1/$1"2" 1>> intel_knl.csv
+    #printf "$1," 1>> intel_knl.csv
+    #./$1/$1"2" 1>> intel_knl.csv
     
-    printf "$1," 1>> intel_rex.csv
-    ./$1/$1"_rex" 1>> intel_rex.csv
+    #printf "$1," 1>> intel_rex.csv
+    #./$1/$1"_rex" 1>> intel_rex.csv
+    
+    CSV=$1".csv"
+    echo "OpenMP (AVX-2),,OpenMP (AVX-512),,Rex," 1>> $CSV
+    ./$1/$1"1" | tr -d '\n' 1>> $CSV
+    printf "," 1>> $CSV
+    
+    ./$1/$1"2" | tr -d '\n' 1>> $CSV
+    printf "," 1>> $CSV
+    
+    ./$1/$1"_rex" | tr -d '\n' 1>> $CSV
+    echo "" 1>> $CSV
 }
 
 # Run
 cd build
 rm ./*.csv
 
-echo "Program,Time (s),Correctness" >> intel_explicit.csv
-echo "Program,Time (s),Correctness" >> intel_knl.csv
-echo "Program,Time (s),Correctness" >> intel_rex.csv
+#echo "Program,Time (s),Correctness" >> intel_explicit.csv
+#echo "Program,Time (s),Correctness" >> intel_knl.csv
+#echo "Program,Time (s),Correctness" >> intel_rex.csv
 
 for d in ./*/
 do
