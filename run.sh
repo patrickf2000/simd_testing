f501d99e294d66233f9b8e617feea4131e2fe413
Patrick Flynn <patrickf4664@protonmail.com>
2022-01-22 08:14:22 -0500

    More improvements to run script

--- run.sh
+++ run.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+# <prog>_serial -> This is the serial, unoptimized version
 # <prog>1 -->    This is compiler-generated (generally AVX512)
 # <prog>2 -->    This is forced AVX-512 (knl flag)
 # <prog>_rex --> This is Rex generated
 #
 # Param 1- the program being run
+# Param 2- the number of times to run each program
 function run_intel() {
     CSV=$1".csv"
+    LAST=$(($2 + 1))
     echo "Serial,,OpenMP (AVX-2),,OpenMP (AVX-512),,Rex," 1>> $CSV
     
-    for i in {1..10}
+    for i in {1..$2}
     do
         ./$1/$1"_serial" | tr -d '\n' 1>> $CSV
         printf "," 1>> $CSV
@@ -23,8 +26,28 @@ function run_intel() {
         ./$1/$1"_rex" | tr -d '\n' 1>> $CSV
         echo "" 1>> $CSV
     done
-    echo "=AVERAGE(A2:A11),,=AVERAGE(C2:C11),,=AVERAGE(E2:E11),,=AVERAGE(G2:G11)" 1>> $CSV
+    echo "=AVERAGE(A2:A$LAST),,=AVERAGE(C2:C$LAST),,=AVERAGE(E2:E$LAST),,=AVERAGE(G2:G$LAST)," 1>> $CSV
 }
+
+# Arm function; this is the same as the Intel
+function run_arm() {
+    echo "ARM: $1 | Running: $2"
+    LAST=$(($2 + 1))
+    
+    echo "=AVERAGE(A2:A$LAST),,=AVERAGE(C2:C$LAST),,=AVERAGE(E2:E$LAST),,=AVERAGE(G2:G$LAST),"
+}
+
+# Make sure we have a command line argument
+if [ -z "$1" ]; then
+    echo "Error: No command line argument specified."
+    echo "Please enter \"intel\" or \"arm\"."
+    exit 1
+fi
+
+RUNS=10
+if [ ! -z "$2" ] ; then
+    RUNS=$2
+fi
 
 # Run
 cd build
@@ -35,7 +58,11 @@ do
     CURRENT=`basename $d`
     if [ $CURRENT != "output" ] ; then
         echo $CURRENT
-        run_intel $CURRENT
+        if [[ $1 == "intel" ]] ; then
+            run_intel $CURRENT $RUNS
+        elif [[ $1 == "arm" ]] ; then
+            run_arm $CURRENT $RUNS
+        fi
     fi
 done
 
@@ -46,3 +73,4 @@ rm output/*
 cp build/*.csv ./output
 
 echo "Done"
+
